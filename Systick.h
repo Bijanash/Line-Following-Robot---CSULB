a029468cf62ccec0f29c564d5be7909159ca9c34
Bijanash <bijanash@gmail.com>
2025-10-14 19:46:37 -0700

    Systick.h

--- Systick.h
+++ Systick.h
@@ -1,10 +1,6 @@
 // Systick.h
 // Line Following Robot Starter program 
-// Created by TYLOR FRANCA 
-// February 2025
-// CECS 497 SPRING 2025
 // California State University, Long Beach
-// Modified by Min He and Mark Joseph Austria
 // 4/25/2025
 
 #include <stdint.h>
@@ -12,5 +8,6 @@
 void SysTick_Init(void);
 
 void Wait_N_MS(uint32_t delay);
+
 
 void Wait_N_US(uint32_t delay);
